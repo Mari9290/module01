81eab912d176b3abdbfc222f848a14c4d7a271a7
marina9290 <marina9290@yandex.ru>
2024-06-23 20:49:39 +0300

    feat:item

--- script.js
+++ script.js
@@ -7,3 +7,13 @@ const productPrice = 1000;
 
 console.log(productName);
 console.log(quantityGoods*productPrice);
+
+{
+    const item = 'milk';
+    const count = 5;
+    const category = 'food';
+    const price = 200;
+
+    console.log(item);
+    console.log(count*price);
+}
