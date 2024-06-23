f233e2acbc8974d5a0ace8517e94d37f1f5bfc5f
marina9290 <marina9290@yandex.ru>
2024-06-23 20:42:17 +0300

    initial

---
+++ script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const productName = 'coffee';
+const quantityGoods = 3;
+const productCategory = 'food';
+const productPrice = 1000;
+
+console.log(productName);
+console.log(quantityGoods*productPrice);
